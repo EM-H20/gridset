98779ba3cccc27bcca55950edc62f4b11cb9a2b6
EM-H20 <ghd0106@gmail.com>
2026-04-25 00:10:57 +0900

    chore : Flutter 의존성 및 플랫폼 플러그인 통합
    - go_router, flutter_riverpod, freezed, flutter_screenutil 등 런타임/빌드 의존성 추가
    - iOS/macOS/Linux/Windows 플랫폼 플러그인 자동 생성 파일 동기화
    - ios/macos Podfile 추가 및 .gitignore 갱신

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import firebase_core
+import firebase_remote_config
+import package_info_plus
+import path_provider_foundation
+import share_plus
+import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
