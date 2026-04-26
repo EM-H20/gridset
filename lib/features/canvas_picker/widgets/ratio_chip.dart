33b0c74551ef65a53d2ca601d7e7ce2349386b61
EM-H20 <ghd0106@gmail.com>
2026-04-27 01:45:40 +0900

    chore : ratio_chip 의 미사용 flutter_screenutil import 제거 (#3)

--- lib/features/canvas_picker/widgets/ratio_chip.dart
+++ lib/features/canvas_picker/widgets/ratio_chip.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../cores/constants/app_colors.dart';
 import '../../../cores/constants/app_spacing.dart';
